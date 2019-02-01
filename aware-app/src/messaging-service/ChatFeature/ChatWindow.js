d9b6a526b08652e9df260541cf615a32cbd892df
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-02-01 01:23:35 -0500

    Improved dynamic message checking.

--- aware-app/src/messaging-service/ChatFeature/ChatWindow.js
+++ aware-app/src/messaging-service/ChatFeature/ChatWindow.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import Message from "./SentMessage"
+import Message from "./Message"
+import SentMessage from "./SentMessage"
 import './ChatWindow.css'
 
 class ChatWindow extends Component {
+  components = {
+    sentMessage: SentMessage,
+    message: Message
+  };
+
   render() {
+    //Can change to this.components.sentMessage to change its rendering.
+    const TagName = this.components.message;
+
     return(
       <div id="chat">
         {this.props.messages.map(message => {
           return (
-            <Message student={message.studentName} avatar={message.avatar} message={message.text} timestamp={message.timestamp}/>
+            <TagName student={message.studentName} avatar={message.avatar} message={message.text} timestamp={message.timestamp}/>
           )
         })}
       </div>
--- aware-app/src/messaging-service/ChatFeature/SentMessage.js
+++ aware-app/src/messaging-service/ChatFeature/SentMessage.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import './SentMessage.css';
 
-class Message extends Component {
+class SentMessage extends Component {
 
   render() {
     const { student, avatar, message, timestamp } = this.props;
 
     return (
-      <div class="message">
-        <div class="col-sm-3 message-timestamp">
+      <div class="sentMessage">
+        <div class="col-sm-3 sentMessage-timestamp">
           {timestamp}
         </div>
-        <div class="col-sm-7 message-text">
+        <div class="col-sm-7 sentMessage-text">
           {message}
         </div>
-        <div class="col-sm-2 message-profile">
-          <img class="message-profile-img" src={process.env.PUBLIC_URL + avatar} alt={student} />
-          <p class="message-profile-name">{student}</p>
+        <div class="col-sm-2 sentMessage-profile">
+          <img class="sentMessage-profile-img" src={process.env.PUBLIC_URL + avatar} alt={student} />
+          <p class="sentMessage-profile-name">{student}</p>
         </div>
       </div>
     );
   }
 }
 
-export default Message;
+export default SentMessage;
