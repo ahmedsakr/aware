70232a41eccd103724dec828dcb513b90153ee6d
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-02-09 18:59:55 -0500

    Connection to Socket.io!

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5001;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+//asdasdasdasdasdasd
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+io.on('connection', function(socket){
+  console.log('Client Has Connected.')
+});
+
+http.listen(5001, function(){
+  console.log('listening on *:5001');
 });
