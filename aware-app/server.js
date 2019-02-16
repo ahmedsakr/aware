a792591605cc0496d3fd5f60c91bb056ab82354d
Josh Campitelli <joshcampitelli@cmail.carleton.ca>
2019-02-16 16:31:51 -0500

    Fixed code from UOttawa Hackathon (#16)

--- aware-app/server.js
+++ aware-app/server.js
@@ -7,22 +7,11 @@ app.get('/server', (req, res) => {
   res.send({ express: 'The Express Server is Connected to React' });
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('Client Has Connected.')
   socket.on('chat message', function(msg){
-    console.log(msg);
-  });
-  socket.on('disconnect', function(){
-    io.emit('chat message', 'user disconnected');
-  });
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+    console.log(msg)
     io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    io.emit('chat message', 'user disconnected');
   });
 });
 
