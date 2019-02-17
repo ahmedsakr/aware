a205ff9fabc9fdc76332ada1a506107247ca2a70
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-02-17 01:29:01 -0500

    Added text

--- aware-app/src/messaging-service/ChatSelector/ChatSelector.js
+++ aware-app/src/messaging-service/ChatSelector/ChatSelector.js
@@ -10,17 +10,17 @@ class ChatSelector extends Component {
       <div id="vertical-menu">
         <h3>Course Discussion</h3>
         <CourseDiscussion src="/messenger-icons/sysc.png" name="sysc 2100" />
-        <CourseDiscussion src="/messenger-icons/sysc.png" name="sysc 2003" />
         <CourseDiscussion src="/messenger-icons/sysc.png" name="sysc 2004" />
-        <CourseDiscussion src="/messenger-icons/sysc.png" name="elec 2501" />
-        <CourseDiscussion src="/messenger-icons/eelc.png" name="elec 2607" />
+        <CourseDiscussion src="/messenger-icons/sysc.png" name="sysc 3110" />
+        <CourseDiscussion src="/messenger-icons/eelc.png" name="elec 2501" />
+        <CourseDiscussion src="/messenger-icons/math.png" name="math 2004" />
 
-        <hr></hr> 
+        <hr></hr>
 
         <h3>Direct Messages</h3>
         <DirectMessage src="/messenger-icons/account.png" name="Josh Campitelli" />
-        <DirectMessage src="/messenger-icons/account.png" name="Josh Campitelli" />
-        <DirectMessage src="/messenger-icons/account.png" name="Josh Campitelli" />
+        <DirectMessage src="/messenger-icons/account.png" name="Ahmed Sakr" />
+        <DirectMessage src="/messenger-icons/account.png" name="Arsalan Sadiq" />
 
       </div>
     );
