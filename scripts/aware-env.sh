95c5f75deebce241e6d63c7f2477564275413612
Ahmed Sakr <ahmad.sakr1997@hotmail.com>
2019-04-13 16:20:26 -0400

    AW-112: Develop a script to automatically populate /etc/hosts with hostnames for aware servers (#23)

---
+++ scripts/aware-env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+AVAILABLE_SERVERS_CONFIG=$SCRIPT_DIR/available.servers
+
+register_hostnames() {
+    echo "You may be prompted to enter your sudo password so that server hostnames for\
+    aware could be registered."
+    sudo sh -c "cat $AVAILABLE_SERVERS_CONFIG >> /etc/hosts"
+}
+
+# Register aware server IP addresses using convienient hostnames.
+grep -q "aware" /etc/hosts
+if [ $? -ne 0 ]; then
+    register_hostnames
+fi
+
+# Useful export variables for potential new scripts.
+export AWARE_SERVER_JIRA="aware-jira"
+export AWARE_SERVER_DEPLOYMENT="aware-deploy-1"
+export AWARE_REPO_SSH="git@github.com:ahmedsakr/aware.git"
