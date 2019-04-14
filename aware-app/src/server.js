229493f14b3fd71c7f55b5c4a460f5d70978b07a
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-04-14 02:45:16 -0400

    Functional - few bugs

--- aware-app/src/server.js
+++ aware-app/src/server.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var chatHistory = {};
+
 // create a GET route
 app.get('/server', (req, res) => {
   res.send({ express: 'The Express Server is Connected to React' });
@@ -18,12 +20,40 @@ io.on('connection', function(socket) {
       socket.leave(currentRoom)
       socket.join(room);
     }
+
+    // If room doesn't have chat history, create room in dictionary
+    if (!(currentRoom in chatHistory)) {
+      if (currentRoom != '') {
+        chatHistory[currentRoom] = []
+        console.log("added room to history" + currentRoom)
+        console.log(chatHistory)
+      }
+    }
+
+    // Return the chat history for current room
+    console.log("=======================")
+    console.log("retrieving from: " + currentRoom)
+    console.log(chatHistory[currentRoom])
+    if (chatHistory[currentRoom] !== undefined) {
+      if (chatHistory[currentRoom].length !== 0) {
+
+        for (var i = 0; i < chatHistory[currentRoom].length; i++) {
+          io.to(socket.id).emit('chat message', chatHistory[currentRoom][i])
+          //Do something
+        }
+      }
+    }
+    console.log("=======================")
   });
 
   socket.on('chat message', function(msg) {
     // get current room of socket to emit message in
     var currentRoom = Object.keys(io.sockets.adapter.sids[socket.id]).filter(item => item!=socket.id);
     io.in(currentRoom).emit('chat message', msg)
+
+    // Append message to history for this room
+    chatHistory[currentRoom].push(msg);
+    console.log(chatHistory)
   });
 });
 
