5dc0875111ebbe954d48b2d09e8701095b8f09da
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-04-14 16:12:02 -0400

    Functioning message history for groups - refactor

--- aware-app/src/server.js
+++ aware-app/src/server.js
@@ -17,44 +17,40 @@ io.on('connection', function(socket) {
 
     // Check if attempting to join current room
     if (currentRoom != room) {
-      socket.leave(currentRoom)
-      socket.join(room);
+      socket.leave(currentRoom, function() {
+        socket.join(room, loadHistory(room));
+      })
+    } else {
+      loadHistory(room);
     }
-
-    // If room doesn't have chat history, create room in dictionary
-    if (!(currentRoom in chatHistory)) {
-      if (currentRoom != '') {
-        chatHistory[currentRoom] = []
-        console.log("added room to history" + currentRoom)
-        console.log(chatHistory)
-      }
-    }
-
-    // Return the chat history for current room
-    console.log("=======================")
-    console.log("retrieving from: " + currentRoom)
-    console.log(chatHistory[currentRoom])
-    if (chatHistory[currentRoom] !== undefined) {
-      if (chatHistory[currentRoom].length !== 0) {
-
-        for (var i = 0; i < chatHistory[currentRoom].length; i++) {
-          io.to(socket.id).emit('chat message', chatHistory[currentRoom][i])
-          //Do something
-        }
-      }
-    }
-    console.log("=======================")
   });
 
   socket.on('chat message', function(msg) {
     // get current room of socket to emit message in
     var currentRoom = Object.keys(io.sockets.adapter.sids[socket.id]).filter(item => item!=socket.id);
-    io.in(currentRoom).emit('chat message', msg)
+    if (currentRoom != '') {
+      io.in(currentRoom).emit('chat message', msg)
 
-    // Append message to history for this room
-    chatHistory[currentRoom].push(msg);
-    console.log(chatHistory)
+      // Append message to history for this room
+      chatHistory[currentRoom].push(msg);
+      console.log(chatHistory)
+    }
   });
+
+  function loadHistory(room) { 
+    // If room doesn't have chat history, create room in dictionary
+    if (!(room in chatHistory)) {
+      if (room != '') {
+        chatHistory[room] = []
+        console.log("added room to history" + room)
+        console.log(chatHistory)
+      }
+    } else {
+      for (var i = 0; i < chatHistory[room].length; i++) {
+        io.to(socket.id).emit('chat message', chatHistory[room][i])
+      }
+    }
+  }
 });
 
 http.listen(5001, function(){
