21ba5f83f86f1368fb830a797961cddc5abd81ef
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-04-14 16:35:01 -0400

    Added getRoom function

--- aware-app/src/server.js
+++ aware-app/src/server.js
@@ -11,7 +11,7 @@ io.on('connection', function(socket) {
   console.log('Client Has Connected, id: ' + socket.id)
 
   socket.on('room', function(room) {
-    var currentRoom = Object.keys(io.sockets.adapter.sids[socket.id]).filter(item => item!=socket.id);
+    var currentRoom = getRoom();
 
     // Check if attempting to join current room
     if (currentRoom != room) {
@@ -22,9 +22,13 @@ io.on('connection', function(socket) {
 
   socket.on('chat message', function(msg) {
     // get current room of socket to emit message in
-    var currentRoom = Object.keys(io.sockets.adapter.sids[socket.id]).filter(item => item!=socket.id);
+    var currentRoom = getRoom();
     io.in(currentRoom).emit('chat message', msg)
   });
+
+  function getRoom() {
+    return Object.keys(io.sockets.adapter.sids[socket.id]).filter(item => item!=socket.id)[0];
+  }
 });
 
 http.listen(5001, function(){
