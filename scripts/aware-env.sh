ecd21abaf6abcd1996235e57e4ab2f9cd89b7ed3
Ahmed Sakr <ahmed@sakr.ca>
2019-05-25 19:51:25 -0400

    AW-304: Update aware-deploy to use the new aware-app.io domain (#36)

--- scripts/aware-env.sh
+++ scripts/aware-env.sh
@@ -3,19 +3,8 @@
 SCRIPT_DIR=`dirname $0`
 AVAILABLE_SERVERS_CONFIG=$SCRIPT_DIR/available.servers
 
-register_hostnames() {
-    echo "You may be prompted to enter your sudo password so that server hostnames for\
-    aware could be registered."
-    sudo sh -c "cat $AVAILABLE_SERVERS_CONFIG >> /etc/hosts"
-}
-
-# Register aware server IP addresses using convienient hostnames.
-grep -q "aware" /etc/hosts
-if [ $? -ne 0 ]; then
-    register_hostnames
-fi
-
 # Useful export variables for potential new scripts.
-export AWARE_SERVER_JIRA="aware-jira"
-export AWARE_SERVER_DEV="aware-dev"
+export AWARE_SERVER_JIRA="jira.aware-app.io:8080"
+export AWARE_SERVER_DEPLOY="deploy.aware-app.io"
+export AWARE_SERVER_TESTLINK="testlink.aware-app.io"
 export AWARE_REPO_SSH="git@github.com:ahmedsakr/aware.git"
