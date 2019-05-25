a65516b5ee2428ae61b9f6216bd368dd0d6bf1ce
ahmedsakr <ahmed@sakr.ca>
2019-05-25 19:37:31 -0400

    Fixed typo

--- scripts/aware-env.sh
+++ scripts/aware-env.sh
@@ -6,5 +6,5 @@ AVAILABLE_SERVERS_CONFIG=$SCRIPT_DIR/available.servers
 # Useful export variables for potential new scripts.
 export AWARE_SERVER_JIRA="jira.aware-app.io:8080"
 export AWARE_SERVER_DEPLOY="deploy.aware-app.io"
-export AWARE_SERVER_TESTLINK="testlik.aware-app.io"
-export AWARE_REPO_SSH="git@github.com:ahmedsakr/aware.git"  
+export AWARE_SERVER_TESTLINK="testlink.aware-app.io"
+export AWARE_REPO_SSH="git@github.com:ahmedsakr/aware.git"
