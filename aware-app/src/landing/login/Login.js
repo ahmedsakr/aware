fb15e8114e5fb274901c68fb8286c91643109e2c
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-05-26 17:15:03 -0400

    update state if checkbox checked

--- aware-app/src/landing/login/Login.js
+++ aware-app/src/landing/login/Login.js
@@ -5,9 +5,11 @@ class Login extends Component {
     constructor() {
         super()
         this.state = {
-            username: undefined
+            username: null,
+            rememberMe: false
         }
-        this.handleChange = this.handleChange.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
     }
 
     render() {
@@ -18,16 +20,19 @@ class Login extends Component {
                 <form>
                     <div class="container">
                         <label id="login-username">Username</label>
-                        <input class="landing-textfield" type="text" value={this.state.username} onChange={this.handleChange}></input>
+                        <input class="landing-textfield" type="text" value={this.state.username} onChange={this.handleUsernameChange}></input>
 
                         <label id="login-password">Password</label>
 
                         <input class="landing-textfield" type="password"></input>
 
-                        <label id="remember-me"><input type="checkbox"/> Remember me</label>
+                        <label id="remember-me">
+                            <input type="checkbox" onChange={this.handleCheckboxChange}/> 
+                            Remember me
+                        </label>
                         <label id="forgot-password"><span>Forgot password?</span></label>
 
-                        <button type="submit" onClick={() => {this.props.setUsername(this.state.username); this.props.loadMessenger()}}>Login</button>
+                        <button type="submit" onClick={() => {this.props.setUsername(this.state.username); this.props.loadMessenger(this.state.rememberMe)}}>Login</button>
 
                         <label id="login-register">Don't have an account? <span onClick={() => this.props.switch()}>register now!</span></label>
                     </div>
@@ -35,8 +40,13 @@ class Login extends Component {
             </div>
         );
     }
-    handleChange(event) {
+
+    handleUsernameChange = (event) => {
         this.setState({username: event.target.value});
+    }
+
+    handleCheckboxChange = (event) => {
+        this.setState({ rememberMe: event.target.checked});
     }
 }
 
