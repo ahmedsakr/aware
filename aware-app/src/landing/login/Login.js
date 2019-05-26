4c288c95a01098197598908733530bbb892801d5
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-05-26 16:52:19 -0400

    remember me checkbox

--- aware-app/src/landing/login/Login.js
+++ aware-app/src/landing/login/Login.js
@@ -21,9 +21,11 @@ class Login extends Component {
                         <input class="landing-textfield" type="text" value={this.state.username} onChange={this.handleChange}></input>
 
                         <label id="login-password">Password</label>
+
                         <input class="landing-textfield" type="password"></input>
 
-                        <label><span>Forgot password?</span></label>
+                        <label id="remember-me"><input type="checkbox"/> Remember me</label>
+                        <label id="forgot-password"><span>Forgot password?</span></label>
 
                         <button type="submit" onClick={() => {this.props.setUsername(this.state.username); this.props.loadMessenger()}}>Login</button>
 
