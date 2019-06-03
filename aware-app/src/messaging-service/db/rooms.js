8b951414ffab6965ca7f3c5a9f844afe4258106e
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-06-02 22:40:40 -0400

    update to join tables and get group_name

--- aware-app/src/messaging-service/db/rooms.js
+++ aware-app/src/messaging-service/db/rooms.js
@@ -1,7 +1,7 @@
 let awaredb = require('../../shared/database/awaredb');
 
 async function getRooms(username) {
-    let sql = `SELECT group_id, name FROM user_groups WHERE username = '${username}'`;
+    let sql = `SELECT group_name AS name FROM user_chats JOIN messenger_group ON user_chats.group_id = messenger_group.group_id WHERE username = '${username}'`;
     return await awaredb.query(sql);
 }
 
