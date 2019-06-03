ccda3e7807df36b3b7552ed9bbb65dbace89e2a3
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-06-02 22:18:14 -0400

    updated long query with proper fields

--- aware-app/src/messaging-service/db/message.js
+++ aware-app/src/messaging-service/db/message.js
@@ -1,16 +1,18 @@
 let awaredb = require('../../shared/database/awaredb');
 let uuid = require ('../../shared/uuid/aware-uuid').uuid;
-let db_table = "message"
+let db_table = "messages"
 
 /**
  * insert message into database.
  * 
  * @param {array} message 
+ * @param {String} groupId 
+ * @param {String} username 
  */
-async function insertMessage(message) {
+async function insertMessage(message, groupId, username) {
     let {text, timestamp} = message;
-    let db_columns = 'message_id, message_content, time_stamp';
-    let user_values = `'${uuid()}', '${text}', '${timestamp}'`;
+    let db_columns = 'message_id, message_content, time_stamp, group_id, username';
+    let user_values = `'${uuid()}', '${text}', '${timestamp}', '${groupId}', '${username}'`;
     await awaredb.query(`INSERT INTO ${db_table} (${db_columns}) VALUES (${user_values})`);
 }
 
@@ -20,7 +22,7 @@ async function insertMessage(message) {
  * @param {String} groupId 
  */
 async function getMessages(groupId) {
-    return await awaredb.query(`SELECT user_accounts.username, message.message_content AS content, message.time_stamp AS timestamp FROM messages JOIN message ON messages.message_id = message.message_id JOIN user_groups ON messages.group_id = user_groups.group_id JOIN user_accounts ON user_groups.username = user_accounts.username WHERE messages.group_id = '${groupId}'`);
+    return await awaredb.query(`SELECT user_accounts.username, messages.message_content AS content, messages.time_stamp AS timestamp FROM messages JOIN user_chats ON messages.group_id = user_chats.group_id JOIN user_accounts ON user_chats.username = user_accounts.username JOIN messenger_group ON user_chats.group_id = messenger_group.group_id WHERE messages.group_id = '${groupId}'`);
 }
 
 module.exports.insertMessage = insertMessage;
