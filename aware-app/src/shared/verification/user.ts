740e374b1c63d7b5d35ad4aa5d5c8aa527a07829
Ahmed Sakr <ahmed@sakr.ca>
2019-06-29 06:59:13 -0500

    Add trailing backslash to validation message

--- aware-app/src/shared/verification/user.ts
+++ aware-app/src/shared/verification/user.ts
@@ -108,7 +108,7 @@ export function getValidationError(field: AccountFields, error: FieldValidationR
         case FieldValidationResult.FIELD_VALUE_ILLEGAL:
             return `${fieldName} value contains illegal characters.`;
         case FieldValidationResult.FIELD_VALUE_OUT_OF_BOUNDS:
-            return `${fieldName} value must be between ${fieldConstraints.minimumSize} and
+            return `${fieldName} value must be between ${fieldConstraints.minimumSize} and\
                     ${fieldConstraints.maximumSize} characters, inclusively.`;
         case FieldValidationResult.FIELD_VALUE_UNDEFINED_OR_NULL:
             return `${fieldName} value not available.`;
