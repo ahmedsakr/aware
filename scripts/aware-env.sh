668d7542efcdaf69ef5ae7ea7e5ef32469bdb1e2
Ahmed Sakr <ahmed@sakr.ca>
2019-07-01 18:44:19 -0500

    Remove deprecated available.servers configuration

--- scripts/aware-env.sh
+++ scripts/aware-env.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR=`dirname $0`
-AVAILABLE_SERVERS_CONFIG=$SCRIPT_DIR/available.servers
-
 # Useful export variables for potential new scripts.
 export AWARE_SERVER_JIRA="jira.aware-app.io:8080"
 export AWARE_SERVER_DEPLOY="deploy.aware-app.io"
