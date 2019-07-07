af3d4561bc8c5ea115cb71ea70a0852532ac4bd6
Ahmed Sakr <ahmed@sakr.ca>
2019-07-07 00:05:04 -0400

    AW-276: Add tooltip for message timestamps instead of displaying the timestamp always (#47)

--- aware-app/src/messaging-service/ChatFeature/ChatWindow/Message/Message.tsx
+++ aware-app/src/messaging-service/ChatFeature/ChatWindow/Message/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Tooltip from 'react-tooltip'
 import './Message.scss'
 import ProfilePicture from '../../Profile/ProfilePicture'
 
@@ -14,10 +15,23 @@ type MessageProps = {
     name: string
 };
 
-type MessageState = { };
+type MessageState = {
+    tooltip: JSX.Element
+};
 
 export default class Message extends React.Component<MessageProps, MessageState> {
+    constructor(props: MessageProps) {
+        super(props);
 
+        this.state = {
+            tooltip: (
+                <Tooltip
+                    effect="solid"
+                    multiline={true}
+                    border={true} />
+            )
+        }
+    }
     produceMessageByThem(content: UserMessageContents): JSX.Element {
         return (
             <div className="col-sm-12 message received-message">
@@ -26,13 +40,11 @@ export default class Message extends React.Component<MessageProps, MessageState>
                     <p className="message-profile-name">{content.username}</p>
                 </div>
 
-                <div className="col-sm-4 message-text received-message-text">
-                    <p>{content.content}</p>
+                <div className="col-sm-8 message-text received-message-text">
+                    <p data-tip={content.timestamp} data-place="right">{content.content}</p>
                 </div>
 
-                <div className="col-sm-2 message-timestamp received-message-timestamp">
-                    <p>{content.timestamp}</p>
-                </div>
+                {this.state.tooltip}
             </div>
         );
     }
@@ -40,21 +52,20 @@ export default class Message extends React.Component<MessageProps, MessageState>
     produceMessageByMe(content: UserMessageContents): JSX.Element {
         return (
             <div className="col-sm-12 message sent-message">
-                <div className="offset-sm-5 col-sm-2 message-timestamp sent-message-timestamp">
-                    <p>{content.timestamp}</p> 
-                </div>
-                <div className="col-sm-4 message-text sent-message-text">
-                    <p>{content.content}</p>
+                <div className="offset-sm-3 col-sm-8 message-text sent-message-text">
+                    <p data-tip={content.timestamp} data-place="left">{content.content}</p>
                 </div>
                 <div className="col-sm-1 message-profile">
                     <ProfilePicture instance="message" activity="online"  picture={content.avatar}/>
                     <p className="message-profile-name">{content.username}</p>
                 </div>
+
+                {this.state.tooltip}
             </div>
         );
     }
     
-    render() {
+    render(): JSX.Element {
         if (this.props.content.username === this.props.name) {
             return this.produceMessageByMe(this.props.content);
         } else {
