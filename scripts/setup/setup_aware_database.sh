b8d27f67e5de54277967e54c07e5292001b07134
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-07-06 23:12:59 -0400

    linked all containers

--- scripts/setup/setup_aware_database.sh
+++ scripts/setup/setup_aware_database.sh
@@ -6,7 +6,7 @@ inform_aligned() {
 
 BASE_DIR=$(dirname `realpath $0`)
 SCHEMA_DIR=$(realpath $BASE_DIR/../../aware-app/src/schemas)
-DOCKER_DIR=$(realpath $BASE_DIR/../../aware-app/src/docker)
+DOCKER_DIR=$(realpath $BASE_DIR/../../aware-app/src/docker/database.Dockerfile)
 AWARE_DATABASE_PORT=$((RANDOM + 1024))
 CONAINER_NAME="aware-db-$AWARE_DATABASE_PORT"
 IMAGE_NAME="aware-database-$AWARE_DATABASE_PORT"
@@ -14,14 +14,15 @@ IMAGE_NAME="aware-database-$AWARE_DATABASE_PORT"
 inform_aligned "Aware Database Port" "$AWARE_DATABASE_PORT"
 
 printf "Building Image for PostgreSQL...\n"
-sudo docker build -t $IMAGE_NAME $DOCKER_DIR >& /dev/null
+sudo docker build -t $IMAGE_NAME --file $DOCKER_DIR . >& /dev/null
 
 printf "Building Containter & Starting it...\n"
 printf "Container ID:"
 sudo docker run -d --rm -p $AWARE_DATABASE_PORT:5432 --name $CONAINER_NAME -v $SCHEMA_DIR:/schemas -it $IMAGE_NAME
 
 # Change database port in App to match conatiner's port
-sed -i -s -e "s/5432/$AWARE_DATABASE_PORT/g" "$BASE_DIR/../../aware-app/package.json"
+#sed -i -s -e "s/5432/$AWARE_DATABASE_PORT/g" "$BASE_DIR/../../aware-app/package.json"
+sed -i -s -e "s/5432/$AWARE_DATABASE_PORT/g" "$BASE_DIR/../../aware-app/src/docker/commands.txt"
 
 printf "Connecting to PSQL & Creating Tables...\n"
 sleep 5s
