720319d83e68e12a9a0f737f233b3d21cb3698cd
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-07-08 23:45:27 -0400

    container port assignment bugged, working tho

--- scripts/setup/setup_aware_app.sh
+++ scripts/setup/setup_aware_app.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-inform_aligned() {
-    printf "%-40s: %s\n" "$1" "$2"
-}
-
 BASE_DIR=$(dirname `realpath $0`)
 
 # if no arguments passed randomize ports, supports local testing
 if [ $# -ne 3 ]; then
-    AWARE_DATABASE_PORT=$((RANDOM + 1024))
-    AWARE_APP_PORT_CLIENT=$((RANDOM + 1024))
-    AWARE_APP_PORT_SERVER=$((RANDOM + 1024))
+    AWARE_DATABASE_PORT=5432 #$((RANDOM + 1024))
+    AWARE_APP_PORT_CLIENT=3000 #$((RANDOM + 1024))
+    AWARE_APP_PORT_SERVER=5001 #$((RANDOM + 1024))
 else
     AWARE_DATABASE_PORT=$1
     AWARE_APP_PORT_CLIENT=$2
@@ -18,17 +14,20 @@ else
     runtime=$4
 fi
 
-#sed -i -s -e "s/PGHOST=localhost/PGHOST=aware-db-$AWARE_DATABASE_PORT/g" "$BASE_DIR/../../aware-app/src"
 $BASE_DIR/setup_aware_database.sh -p $AWARE_DATABASE_PORT
 
+# Modify package json to listen to contianer IP
+sed -i -s -e "s/PGHOST=/PGHOST=aware-db-$AWARE_DATABASE_PORT/g" "$BASE_DIR/../../aware-app/package.json"
+
 printf "Building Image for Server...\n"
-sudo docker build -t server --file $BASE_DIR/../../aware-app/src/docker/server.Dockerfile $BASE_DIR/../../aware-app/ >& /dev/null
+sudo docker build -t server-$AWARE_APP_PORT_SERVER --file $BASE_DIR/../../aware-app/src/docker/server.Dockerfile $BASE_DIR/../../aware-app/ >& /dev/null
 printf "Starting Server Container...\n"
-sudo docker run -v $BASE_DIR/../../aware-app:/app -v /app/node_modules --name server --link=aware-db-$AWARE_DATABASE_PORT:database -p 5001:5001 --rm server >& /dev/null &
+sudo docker run -v $BASE_DIR/../../aware-app:/app -v /app/node_modules --name server-$AWARE_APP_PORT_SERVER --link=aware-db-$AWARE_DATABASE_PORT:database -p $AWARE_APP_PORT_CLIENT:5001 --rm server-$AWARE_APP_PORT_SERVER >& /dev/null &
 printf "Building Image for Client...\n"
-sudo docker build -t client --file $BASE_DIR/../../aware-app/src/docker/client.Dockerfile $BASE_DIR/../../aware-app/ >& /dev/null
+sudo docker build -t client-$AWARE_APP_PORT_CLIENT --file $BASE_DIR/../../aware-app/src/docker/client.Dockerfile $BASE_DIR/../../aware-app/ >& /dev/null
 printf "Starting Client Container...\n"
-sudo docker run -v $BASE_DIR/../../aware-app:/app -v /app/node_modules --name client --link=server:server -p 3000:3000 --rm client >& /dev/null &
+sudo docker run -v $BASE_DIR/../../aware-app:/app -v /app/node_modules --name client-$AWARE_APP_PORT_CLIENT --link=server-$AWARE_APP_PORT_SERVER:server -p $AWARE_APP_PORT_CLIENT:3000 --rm client-$AWARE_APP_PORT_CLIENT #>& /dev/null &
 
 sleep 5s
 printf "Successfully created docker containers\n"
+printf "Available at: http://localhost:$AWARE_APP_PORT_CLIENT/ \n"
