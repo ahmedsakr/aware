58d586ac2e85b999ca1f546148c424ebcaa947ad
ahmedsakr <ahmed@sakr.ca>
2019-07-24 19:05:43 -0400

    Add button for creating new direct message

--- aware-app/src/messaging-service/ChatSelector/DirectMessages/DirectMessages.tsx
+++ aware-app/src/messaging-service/ChatSelector/DirectMessages/DirectMessages.tsx
@@ -14,6 +14,12 @@ export default class DirectMessages extends React.Component<DirectMessagesProps,
         return (
             <div id="direct-messages">
                 <input id="textfield" placeholder="Search Messages..." />
+
+                <div id="direct-messages-new">
+                    <span className="fa fa-plus" aria-hidden="true"></span>
+                    <p>Start a direct message</p>
+                </div>
+
                 <DirectMessage
                     selectDirectMessage={this.props.selectDirectMessage}
                     room=""
