f67316864a57158b667079a3d48f0b160b8d3691
ahmedsakr <ahmed@sakr.ca>
2019-07-27 22:02:18 -0400

    Add courses related users database call

---
+++ aware-app/src/messaging-service/db/userRelations.ts
@@ -0,0 +1,22 @@
+import awaredb from '../../shared/database/awaredb'
+
+/**
+ * Retrieves all users that are connected with the given user through
+ * at least one common course.
+ *
+ * @param username 
+ */
+export async function getRelatedUsers(username: string): Promise<Object[]> {
+
+    let sql: string = ` SELECT DISTINCT
+                            them.username from user_courses
+                        AS me
+                        JOIN user_courses
+                        AS them
+                        ON
+                            me.course_id = them.course_id
+                        WHERE
+                            me.username=$1 AND them.username != $1;`
+    
+    return await awaredb(sql, [`${username}`]);
+}
