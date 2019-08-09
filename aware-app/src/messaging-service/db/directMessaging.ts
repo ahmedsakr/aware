a83146af05aa64d34f1d905781351b72eb18a4e2
ahmedsakr <ahmed@sakr.ca>
2019-08-08 22:13:20 -0400

    Start to define the direct messaging backend

---
+++ aware-app/src/messaging-service/db/directMessaging.ts
@@ -0,0 +1,10 @@
+
+/**
+ * Start a new direct message with a related user.
+ *
+ * @param myUsername The username of the user submitting this request
+ * @param theirUsername The target user to start the direct message with
+ */
+export async function startDirectMessage(myUsername: string, theirUsername: string) {
+
+}
