7faae23895f9938470da704076d7c16036b441c6
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-09-06 22:33:24 -0400

    AW-271: Added Room type

--- aware-app/src/messaging-service/db/rooms.ts
+++ aware-app/src/messaging-service/db/rooms.ts
@@ -1,6 +1,11 @@
 import awaredb from '../../shared/database/awaredb';
+import { AccountField } from '../../shared/verification/user';
 
-export default async function getRooms(username: string): Promise<Object[]> {
+export type Room = {
+    group_id: string
+}
+
+export async function getRooms(username: AccountField): Promise<Room[]> {
     let sql =   `SELECT group_name AS name, group_icon AS icon, user_chats.group_id FROM user_chats
                 JOIN messenger_group ON user_chats.group_id = messenger_group.group_id
                 WHERE username = $1`;
