820cf8758f846a43f4cde3696576185c888efbf0
Josh Campitelli <joshcampitelli@cmail.carleton.ca>
2019-09-14 21:30:53 -0400

    AW-282: Implement full-size background image slider for Landing Page (#48)

--- aware-app/src/landing/Landing.tsx
+++ aware-app/src/landing/Landing.tsx
@@ -5,6 +5,12 @@ import './Landing.scss'
 import Login from './login/Login'
 import Register from './register/Register'
 
+import Slider, {Settings}from 'react-slick'
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css'
+import './LandingSlider.scss'
+
 type LandingProps = {
     loadMessenger: (username: string) => void;
     socket: SocketIOClient.Socket
@@ -84,12 +90,40 @@ export default class Landing extends React.Component<LandingProps, LandingState>
     render() {
         return (
             <div className="container-fluid aware-container">
-                <div id="landing-canvas" className="aware-column row">
-                    <div className="col-3 offset-3">
+                <div id="landing-canvas">
+                    <LandingSlider/>
+                    <div id='landing-form' className="col-3 offset-3">
                         {this.displayCurrentForm()}
                     </div>
                 </div>
             </div>
         );
     }
+}
+
+type LandingSliderProps = {};
+
+const LandingSlider: React.FC<LandingSliderProps> = (props: LandingSliderProps) => {
+    let settings: Settings = {
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        fade: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 5000,
+        speed: 1000,
+        lazyLoad: 'ondemand'
+    };
+    return (
+        <div>
+            <Slider {...settings}>
+                <div><img className='slide-item' src={process.env.PUBLIC_URL + "/shrek.jpg"} alt="Shrek" /></div>
+                <div><img className='slide-item' src={process.env.PUBLIC_URL + "/test1.jpg"} alt="Image 1" /></div>
+                <div><img className='slide-item' src={process.env.PUBLIC_URL + "/test2.jpg"} alt="Image 2" /></div>
+                <div><img className='slide-item' src={process.env.PUBLIC_URL + "/test3.jpg"} alt="Image 3" /></div>
+                <div><img className='slide-item' src={process.env.PUBLIC_URL + "/test4.jpg"} alt="Image 4" /></div>
+            </Slider>
+        </div>
+    );
 }
