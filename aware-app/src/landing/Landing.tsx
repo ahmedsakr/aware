a6ba1a08122f192af714a2044394fcaa1984475e
Josh Campitelli <joshcampitelli@cmail.carleton.ca>
2019-10-20 19:11:11 -0400

    Update background images

--- aware-app/src/landing/Landing.tsx
+++ aware-app/src/landing/Landing.tsx
@@ -100,9 +100,9 @@ const LandingSlider: React.FC<SimpleSliderProps> = (props: SimpleSliderProps) =>
     return (
         <div id="test-slider" className="swiper-container">
             <div className="swiper-wrapper">
-                <div className="text-dark swiper-slide">Slide 1</div>
-                <div className="bg-success swiper-slide">Slide 2</div>
-                <div className="swiper-slide">Slide 3</div>
+                <div className="swiper-slide" id="slide-1"></div>
+                <div className="swiper-slide" id="slide-2"></div>
+                <div className="swiper-slide" id="slide-3"></div>
             </div>
             <div className="swiper-pagination"></div>
             <div className="swiper-button-prev"></div>
